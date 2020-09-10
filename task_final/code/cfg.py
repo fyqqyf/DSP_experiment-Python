d91c35a68c263208fdb4cc342fa344e33e06314e
umu-chert <yuqianfu@whu.edu.cn>
2020-09-10 15:29:21 +0800

    test

---
+++ task_final/code/cfg.py
@@ -0,0 +1,14 @@
+'''
+Function:
+	config file.
+Author:
+	FYQ
+'''
+'''pencil stroke generation'''
+kernel_size_scale = 1 / 40
+stroke_width = 1
+'''pencil tone drawing'''
+color_depth = 1
+weights_color = [62, 30, 5]
+weights_gray = [76, 22, 2]
+texture_path = './textures/6.jpg'
